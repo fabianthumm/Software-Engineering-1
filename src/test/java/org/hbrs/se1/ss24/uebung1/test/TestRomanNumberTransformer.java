8bd551874afd9522080b7725c1e1e49477df672f
Fabian Thumm <56392874+fabianthumm@users.noreply.github.com>
2024-04-15 15:07:01 +0200

    Add more Tests

--- src/test/java/org/hbrs/se1/ss24/uebung1/test/TestRomanNumberTransformer.java
+++ src/test/java/org/hbrs/se1/ss24/uebung1/test/TestRomanNumberTransformer.java
@@ -8,6 +8,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class TestRomanNumberTransformer {
 
     @Test
+    void testMinInteger() {
+        RomanNumberTransformer transformer = new RomanNumberTransformer();
+        String result = transformer.transformNumber(Integer.MIN_VALUE);
+        assertEquals("Eingabe ausserhalb des Wertebereichs!", result);
+    }
+
+    @Test
+    void testMaxInteger() {
+        RomanNumberTransformer transformer = new RomanNumberTransformer();
+        String result = transformer.transformNumber(Integer.MAX_VALUE);
+        assertEquals("Eingabe ausserhalb des Wertebereichs!", result);
+    }
+
+    @Test
     void testNegativeNumber() {
         RomanNumberTransformer transformer = new RomanNumberTransformer();
         String result = transformer.transformNumber(-17);
@@ -26,6 +40,20 @@ class TestRomanNumberTransformer {
         RomanNumberTransformer transformer = new RomanNumberTransformer();
         String result = transformer.transformNumber(1245);
         assertEquals("MCCXLV", result);
+    }
+
+    @Test
+    void testValidNumberMin() {
+        RomanNumberTransformer transformer = new RomanNumberTransformer();
+        String result = transformer.transformNumber(1);
+        assertEquals("I", result);
+    }
+
+    @Test
+    void testValidNumberMax() {
+        RomanNumberTransformer transformer = new RomanNumberTransformer();
+        String result = transformer.transformNumber(3000);
+        assertEquals("MMM", result);
     }
 
     @Test
