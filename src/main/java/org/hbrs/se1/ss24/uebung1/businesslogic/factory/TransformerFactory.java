b6aa28eae7e7fc9961ae71d38823e416c811621c
Fabian Thumm <56392874+fabianthumm@users.noreply.github.com>
2024-04-18 11:27:40 +0200

    Add Branch Übung-1

--- src/main/java/org/hbrs/se1/ss24/uebung1/businesslogic/factory/TransformerFactory.java
+++ src/main/java/org/hbrs/se1/ss24/uebung1/businesslogic/factory/TransformerFactory.java
@@ -1,10 +1,15 @@
 package org.hbrs.se1.ss24.uebung1.businesslogic.factory;
 
+import org.hbrs.se1.ss24.uebung1.businesslogic.GermanFormatNumberTransformer;
 import org.hbrs.se1.ss24.uebung1.businesslogic.NumberTransformer;
 import org.hbrs.se1.ss24.uebung1.businesslogic.RomanNumberTransformer;
 
 public class TransformerFactory {
     public static NumberTransformer createRomanNumberTransformer() {
         return new RomanNumberTransformer();
+    }
+
+    public static NumberTransformer createGermanFormatNumberTransformer() {
+        return new GermanFormatNumberTransformer();
     }
 }
